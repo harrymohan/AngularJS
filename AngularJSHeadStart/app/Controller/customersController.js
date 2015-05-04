3d949d3c2dd20cf4dd96be445d71f747fca2efbd
unknown <erharmohan@gmail.com>
2015-05-04 02:19:09 -0700

    Adding Service

--- AngularJSHeadStart/app/Controller/customersController.js
+++ AngularJSHeadStart/app/Controller/customersController.js
@@ -1,46 +1,24 @@
-app.controller('customersController',function(){
-	//alert('customer controller init');
+app.controller('customersController',function(customersFactory){
+	
 	this.sortBy ='name';
-	this.Customers =[{id:1,name:'Jason',age:'35',OrderedPlaced:'12/01/2012',
-                   orders:[
-                       {
-                           id:1,
-                           product:'shoes',
-                           total:9.94
-                       }
-                           
-                   ]
-                  },
-                  {id:2,name:'Sally',age:'32',OrderedPlaced:'06/21/2010',
-                  orders:[
-                       {
-                           id:2,
-                           product:'baseball',
-                           total:19.94
-                       }
-                           
-                   ]
-                  },
-                  {id:3,name:'Harry',age:'29',OrderedPlaced:'01/04/2007',
-                  orders:[
-                       {
-                           id:3,
-                           product:'Perfume',
-                           total:123.21
-                       }
-                           
-                   ]
-                  }];
 	
+    var vmc = this;
+    
+    vmc.customers = [];
+    
+    function init(){
+        vmc.customers = customersFactory.getCustomers();
+    }
 	
+	init();
 	this.doSort = function(propName){
-		//alert(propName);
+	
 		this.sortBy = propName;
 		this.reverse = !this.reverse;
 	}
-	this.total = function(total){
-		//alert('total'+ 	this.People.length);
-		return this.Customers.length;
+	this.total = function(){
+	
+		return vmc.customers.length;
 	}
 		
 });
--- AngularJSHeadStart/app/Controller/ordersController.js
+++ AngularJSHeadStart/app/Controller/ordersController.js
@@ -1,56 +1,14 @@
-app.controller('ordersController',function($routeParams){
+app.controller('ordersController',function($routeParams,customersFactory){
 	//alert('orders controller init');
     var customerId = $routeParams.customerId;
 	
    // alert('customerID'+customerId);
     var vm = this; 
-   vm.orders = null;
-	vm.Customers =[{id:1,name:'Jason',age:'35',OrderedPlaced:'12/01/2012',
-                   orders:[
-                       {
-                           id:1,
-                           product:'shoes',
-                           total:9.94
-                       }
-                           
-                   ]
-                  },
-                  {id:2,name:'Sally',age:'32',OrderedPlaced:'06/21/2010',
-                  orders:[
-                       {
-                           id:2,
-                           product:'baseball',
-                           total:19.94
-                       }
-                           
-                   ]
-                  },
-                  {id:3,name:'Harry',age:'29',OrderedPlaced:'01/04/2007',
-                  orders:[
-                       {
-                           id:3,
-                           product:'Perfume',
-                           total:123.21
-                       }
-                           
-                   ]
-                  }
-                  
-                 ];
+    vm.customer = [];
+    
 	    function init(){
-        //alert('init fn: '+vm.Customers.length);
-        var len = vm.Customers.length;
-        //alert(vm.Customers[0].id);
-        for(var i=0;i<len;i++)
-        {
-            if(vm.Customers[i].id === parseInt(customerId))
-            {
-                vm.orders = vm.Customers[i].orders;
-                //alert('inside eq');
-                break;
-            }
-        }
-      
+        vm.customer = customersFactory.getCustomer(customerId);
+          
     }
     
   init();
